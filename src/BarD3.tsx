5abbfc8f823e58a96a7a236fa00aab9d5df0083b
Vasiliy Solomatin <tumba.90@mail.ru>
2022-08-29 16:58:59 +0300

    add event and color

--- src/BarD3.tsx
+++ src/BarD3.tsx
@@ -1,26 +1,8 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 import React, { useEffect, createRef } from "react";
 import { styled } from "@superset-ui/core";
 import { BarD3Props, BarD3StylesProps } from "./types";
 import * as d3 from "d3";
-// import BarD3Component from "./BarD3Component";
+import { schemeSet3, transition } from "d3";
 
 // The following Styles component is a <div> element, which has been styled using Emotion
 // For docs, visit https://emotion.sh/docs/styled
@@ -30,7 +12,7 @@ import * as d3 from "d3";
 // https://github.com/apache-superset/superset-ui/blob/master/packages/superset-ui-core/src/style/index.ts
 
 const Styles = styled.div<BarD3StylesProps>`
-  background-color: ${({ theme }) => theme.colors.secondary.light2};
+  //background-color: ${({ theme }) => theme.colors.secondary.light2};
   padding: ${({ theme }) => theme.gridUnit * 4}px;
   border-radius: ${({ theme }) => theme.gridUnit * 2}px;
   height: ${({ height }) => height}px;
@@ -60,28 +42,10 @@ const Styles = styled.div<BarD3StylesProps>`
  *  * FormData (your controls!) provided as props by transformProps.ts
  */
 
-function BarD3Component() {
-  const w = 500;
-  const h = 300;
-  const padding = 30;
-
-  const svg = d3
-    .select("slice_container")
-    .append("svg")
-    .attr("width", w)
-    .attr("height", h);
-
-  return (
-    <div className="test">
-      <span>Hello</span>
-    </div>
-  );
-}
-
 export default function BarD3(props: BarD3Props) {
   // height and width are the height and width of the DOM element as it exists in the dashboard.
   // There is also a `data` prop, which is, of course, your DATA 🎉
-  const { data, height, width } = props;
+  const { /* data, */ height, width } = props;
 
   const rootElem = createRef<HTMLDivElement>();
   // Often, you just want to get a hold of the DOM and go nuts.
@@ -111,9 +75,10 @@ export default function BarD3(props: BarD3Props) {
     "year": 2008,
     "count": 1428 */
 
-    const dataArray = [20, 40, 50, 60];
+    const dataArray = [20, 15, 35, 20, 55, 5, 19, 40, 50, 60];
     //const width = 500;
     //const height = 500;
+    const padding = 5;
     const widthScale = d3.scaleLinear([0, 60], [0, width]);
     const color = d3.scaleLinear([0, 60], ["blue", "red"]);
 
@@ -121,7 +86,6 @@ export default function BarD3(props: BarD3Props) {
       ? element.select("svg")
       : element.append("svg").attr("width", 50).attr("height", 50); */
 
-    console.log("isExist", element.select(".MyChart").node());
     if (element.select(".MyChart")) {
       element.select(".MyChart").remove();
     }
@@ -133,18 +97,55 @@ export default function BarD3(props: BarD3Props) {
       .attr("class", "MyChart")
       .append("g")
       .attr("transform", "translate(20,0)");
-    const bars = canvas
+
+    function calcY(d: any, i: number) {
+      return Math.floor(height / dataArray.length) * i;
+    }
+
+    const heightRect = height / dataArray.length - padding;
+
+    const rect = canvas
       .selectAll("rect")
       .data(dataArray)
       .enter()
+      .append("g")
+      .attr("class", "rectItem")
       .append("rect")
-      .attr("fill", (d) => color(d))
-      .attr("width", (d) => widthScale(d))
-      .attr("height", 30)
+      .attr("fill", "blue")
+      .attr("width", 0)
+      .attr("height", heightRect)
       .attr("y", (d, i) => {
-        return i * 50;
+        return calcY(d, i);
       });
 
+    const text = canvas
+      .selectAll(".rectItem")
+      .append("text")
+      .text((d: any) => d)
+      .attr("x", 20)
+      .attr("y", (d, i) => {
+        return calcY(d, i) + (heightRect + 2 * padding) / 2;
+      });
+
+    rect
+      .transition()
+      .attr("width", (d) => widthScale(d))
+      .duration(1000);
+    rect.on("mouseenter", function () {
+      //d3.select(this).node().parentNode
+      d3.select(this).transition().duration(300).attr("fill", "red");
+      //d3.select(this).select("text").attr("fill", "white");
+
+      //d3.select(this.parentNode)
+      console.log(d3.select(this).node().parentNode);
+
+      /* const parent = child.select(function() {
+        return this.closest(".parent");  // Get the closest parent matching the selector string.
+      }); */
+    });
+    rect.on("mouseleave", function () {
+      d3.select(this).transition().duration(300).attr("fill", "blue");
+    });
     //console.log("data", data);
 
     //console.log("isExistClass", d3Root.select("MyChart").node());
@@ -165,8 +166,8 @@ export default function BarD3(props: BarD3Props) {
       height={height}
       width={width}
     >
-      <h3>{props.headerText}</h3>
-      {/* <pre>${JSON.stringify(data, null, 2)}</pre> */}
+      {/*<h3>{props.headerText}</h3>
+       <pre>${JSON.stringify(data, null, 2)}</pre> */}
     </Styles>
   );
 }
